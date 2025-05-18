0142d9ad1cebd56a4c1e7d448372faad614942b0
Duncan McGregor <duncan@oneeyedmen.com>
2025-05-18 15:33:24 +0100

    VickreyAuction.decideWinner uses rules.decideWinner

--- src/main/kotlin/com/example/auction/model/AuctionRules.kt
+++ src/main/kotlin/com/example/auction/model/AuctionRules.kt
@@ -1,5 +1,33 @@
 package com.example.auction.model
 
 enum class AuctionRules {
-    Blind, Vickrey, Reverse
+    Blind {
+        override fun decideWinner(auction: Auction): AuctionWinner? {
+            TODO("Not yet implemented")
+        }
+    }, Vickrey {
+        override fun decideWinner(auction: Auction): AuctionWinner? {
+            return vickreyActionWinner(auction)
+        }
+    }, Reverse {
+        override fun decideWinner(auction: Auction): AuctionWinner? {
+            TODO("Not yet implemented")
+        }
+    };
+
+    abstract fun decideWinner(auction: Auction): AuctionWinner?
 }
+
+fun vickreyActionWinner(auction: Auction): AuctionWinner? =
+    auction.bids
+        .associateBy { it.buyer }
+        .values
+        .sortedByDescending { it.amount }
+        .take(2)
+        .run {
+            when {
+                isEmpty() -> null
+                last().amount < auction.reserve -> null
+                else -> AuctionWinner(first().buyer, last().amount)
+            }
+        }
--- src/main/kotlin/com/example/auction/model/VickreyAuction.kt
+++ src/main/kotlin/com/example/auction/model/VickreyAuction.kt
@@ -15,22 +15,9 @@ class VickreyAuction(
     override var id: AuctionId = AuctionId.NONE,
     override var bids: MutableList<Bid> = mutableListOf(),
     override var state: AuctionState = open,
-    override var winner: AuctionWinner? = null
+    override var winner: AuctionWinner? = null,
 ) : Auction() {
     override val rules = Vickrey
-    
-    override fun decideWinner(): AuctionWinner? {
-        return bids
-            .associateBy { it.buyer }
-            .values
-            .sortedByDescending { it.amount }
-            .take(2)
-            .run {
-                when {
-                    isEmpty() -> null
-                    last().amount < reserve -> null
-                    else -> AuctionWinner(first().buyer, last().amount)
-                }
-            }
-    }
+    override fun decideWinner(): AuctionWinner? = rules.decideWinner(this)
+
 }
